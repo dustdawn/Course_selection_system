8e50c0c6c8c8679b8c69316ce204793679448f2a
luobin <1667918791@qq.com>
2019-10-20 15:21:14 +0800

    后台功能实现

---
+++ src/main/java/cn/njit/dao/NoticeMapper.java
@@ -0,0 +1,23 @@
+package cn.njit.dao;
+
+import cn.njit.entry.Notice;
+
+import java.util.List;
+
+public interface NoticeMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(Notice record);
+
+    int insertSelective(Notice record);
+
+    Notice selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(Notice record);
+
+    int updateByPrimaryKeyWithBLOBs(Notice record);
+
+    int updateByPrimaryKey(Notice record);
+
+    List<Notice> findList();
+}
---
+++ src/main/java/cn/njit/entry/Notice.java
@@ -0,0 +1,33 @@
+package cn.njit.entry;
+
+public class Notice {
+    private String id;
+
+    private Integer delFlag;
+
+    private String text;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Integer getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(Integer delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text == null ? null : text.trim();
+    }
+}
